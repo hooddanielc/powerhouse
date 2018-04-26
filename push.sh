e1078cf454f62df176d1b4ac319a6158707f79bb
Daniel Hood <hood.danielc@gmail.com>
2018-04-25 20:53:41 -0700

    Fix tagging

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 TAG=$(date -u +"%Y-%m-%dt%H.%M.%Sz")
-docker tag dhoodlum/powerhouse $TAG
-docker push dhoodlum/powerhouse
+docker tag dhoodlum/powerhouse dhoodlum/powerhouse:$TAG
 docker push dhoodlum/powerhouse:$TAG
+docker push dhoodlum/powerhouse
